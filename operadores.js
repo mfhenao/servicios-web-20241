0954a79ed17e100376cc3c670e14c4c71aecbd65
mfhenao <mf.henao10@ciaf.edu.co>
2024-03-13 20:50:39 -0500

    creamos operadores logicos

---
+++ operadores.js
@@ -0,0 +1,36 @@
+let counter=1000;
+//suma 
+counter= counter + 20;
+console.log(counter)
+counter +=30;
+console.log(counter)
+
+//resta
+counter= counter - 20;
+console.log(counter)
+counter -= 30;
+console.log(counter)
+
+//multiplicacion
+counter= counter * 3;
+console.log(counter)
+counter *= 4;
+console.log(counter)
+
+//division
+counter= counter / 5;
+console.log(counter)
+counter /= 4;
+console.log(counter)
+
+//residuo
+counter= counter%3;
+console.log(counter)
+counter%=3;
+console.log(counter)
+
+//exponenciacion
+counter=counter**3;
+console.log(counter)
+counter **=3;
+console.log(counter)
